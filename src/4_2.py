bd37d4d14c48127b6cf777b7bf143db2902d1e88
silencht&LY <silencht@qq.com>
2020-12-22 18:45:22 +0800

    修改4_2注释的一些小错误

--- src/4_2.py
+++ src/4_2.py
@@ -29,7 +29,7 @@ cv.imshow("rst",rst)
 cv.waitKey()
 cv.destroyAllWindows()
 
-#利用重映射对图像进行X轴翻转
+#利用重映射对图像进行x轴翻转
 image = cv.imread("C:/Users/silencht/Desktop/Opencv_Learn/img/lena.png")
 w ,h = image.shape[:2] #获取图像大小信息
 x = np.zeros((w,h),np.float32)
@@ -37,14 +37,14 @@ y = np.zeros((w,h),np.float32)
 for i in range(w):
     for j in range(h):
         x.itemset((i,j),j)# x--W,y--H,所以x代表列，y代表行
-        y.itemset((i,j),w-1-i)#绕X轴翻转，实际上是映射过程中X坐标轴的值不变，Y坐标轴的值以X轴为对称轴进行交换
+        y.itemset((i,j),w-1-i)#绕x轴翻转，实际上是映射过程中x坐标轴的值不变，y坐标轴的值以x轴为对称轴进行交换
 rst = cv.remap(image,x,y,cv.INTER_LINEAR)#将图像重映射(这里为绕X轴翻转)至一个新的图像
 cv.imshow("image",image)
 cv.imshow("rst",rst)
 cv.waitKey()
 cv.destroyAllWindows()
 
-#利用重映射对图像进行Y轴翻转
+#利用重映射对图像进行y轴翻转
 image = cv.imread("C:/Users/silencht/Desktop/Opencv_Learn/img/lena.png")
 w ,h = image.shape[:2] #获取图像大小信息
 x = np.zeros((w,h),np.float32)
@@ -52,7 +52,7 @@ y = np.zeros((w,h),np.float32)
 for i in range(w):
     for j in range(h):
         x.itemset((i,j),h-1-j)# x--W,y--H,所以x代表列，y代表行
-        y.itemset((i,j),i)#绕X轴翻转，实际上是映射过程中y坐标轴的值不变，x坐标轴的值以y轴为对称轴进行交换
+        y.itemset((i,j),i)#绕y轴翻转，实际上是映射过程中y坐标轴的值不变，x坐标轴的值以y轴为对称轴进行交换
 rst = cv.remap(image,x,y,cv.INTER_LINEAR)#将图像重映射(这里为绕y轴翻转)至一个新的图像
 cv.imshow("image",image)
 cv.imshow("rst",rst)
@@ -67,7 +67,7 @@ y = np.zeros((w,h),np.float32)
 for i in range(w):
     for j in range(h):
         x.itemset((i,j),h-1-j)# x--W,y--H,所以x代表列，y代表行
-        y.itemset((i,j),w-1-i)#绕X/y轴翻转
+        y.itemset((i,j),w-1-i)#绕x/y轴翻转
 rst = cv.remap(image,x,y,cv.INTER_LINEAR)#将图像重映射(这里为绕x/y轴翻转)至一个新的图像
 cv.imshow("image",image)
 cv.imshow("rst",rst)
